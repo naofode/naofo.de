1fb9c3063cd566b706cfc8b96194836a5fc2c111
Pedro Moraes <pedromoraes@gmail.com>
2014-07-28 13:12:26 -0300

    scriptlet form charset

--- index.php
+++ index.php
@@ -159,7 +159,7 @@ if ($code) {
             <footer class="wrapper">
                 <a href="https://github.com/pedromoraes/naofo.de">código fonte</a>
                 &nbsp;&nbsp;-&nbsp;&nbsp;
-                <a title="botao naofo.de" href='javascript:var d=document,b=d.body,div=d.createElement("div");div.innerHTML="<form action=\"http://naofo.de\" method=\"post\" target=\"_blank\"><input name=\"url\"><input name=\"title\"></form>";div.style.display="none";b.appendChild(div);var f=div.children[0];f["url"].value=window.location.href;f["title"].value=d.title;f.submit();' onclick="return false;">botão naofo.de</a> (arraste para sua barra de favoritos e clique quando estiver na página que deseja encurtar)
+                <a title="botao naofo.de" href='javascript:var d=document,b=d.body,div=d.createElement("div");div.innerHTML="<form accept-charset=\"UTF-8\" action=\"http://naofo.de\" method=\"post\" target=\"_blank\"><input name=\"url\"><input name=\"title\"></form>";div.style.display="none";b.appendChild(div);var f=div.children[0];f["url"].value=window.location.href;f["title"].value=d.title;f.submit();' onclick="return false;">botão naofo.de</a> (arraste para sua barra de favoritos e clique quando estiver na página que deseja encurtar)
             </footer>
         </div>
 
