db090f99f5f53b58b2165ab971f39688fadc9c38
Pedro Moraes <pedromoraes@gmail.com>
2014-08-03 14:01:10 -0300

    shellfix

--- capture.sh
+++ capture.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 min_size=1024
 max_size=52428800
-wkhtmltoimage --crop-h 14000 --width 1280 $1 $2
+wkhtmltoimage --stop-slow-scripts --crop-h 14000 --width 1280 $1 $2
 file_size=$(stat -c%s $2)
 if [[ ( $file_size -lt $min_size ) ]]; then
-    wkhtmltoimage --crop-h 14000 --custom-header 'User-Agent' 'Mozilla/5.0 (Linux; U; Android 4.0.3; ko-kr; LG-L160L Build/IML74K) AppleWebkit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30' --custom-header-propagation --enable-javascript --javascript-delay 1500 --no-stop-slow-scripts --debug-javascript --load-error-handling ignore --width 1280 "$1?&retry" $2
+    wkhtmltoimage --crop-h 14000 --custom-header 'User-Agent' 'Mozilla/5.0 (Linux; U; Android 4.0.3; ko-kr; LG-L160L Build/IML74K) AppleWebkit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30' --custom-header-propagation --enable-javascript --javascript-delay 1500 --stop-slow-scripts --debug-javascript --load-error-handling ignore --width 1280 "$1?&retry" $2
     file_size=$(stat -c%s $2)
     if [[ ( $file_size -lt $min_size ) ]]; then
     	rm $2
