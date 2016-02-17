259414e4bcd9182566b5e96cd6026a9a218ac05b
Desobediente Civil <desci@riseup.net>
2016-02-17 18:28:18 -0200

    Variável que eu tinha apagado sem querer

--- static.php
+++ static.php
@@ -1,7 +1,8 @@
 <?php
 
-$site = "nao.usem.xyz";
+$site = 'nao.usem.xyz';
 $url = "http://$site/";
+$title = "<strong>$site</strong> | encurtador higiênico de chorume";
 
 // Variáveis secretas
 include("private.php");
